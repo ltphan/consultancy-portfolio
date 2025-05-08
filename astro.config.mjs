0ed75e5986a2646915c890d4bb836b96648c6c7e
Linh Phan <phanthuclinh@gmail.com>
2025-05-08 09:00:15 -0700

    layout for consultancy business website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+// Import the Tailwind Vite plugin directly
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    react()
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 });
